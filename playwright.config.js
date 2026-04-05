b4b62d0e69f42e54eb1c1812c1ae6f3e4cec20db
LyonInformatic <Davidmarchantb@gmail.com>
2026-04-05 18:44:54 -0400

    chore: configurar ejecución visual con slowMo

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,11 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+
+    headless: false,
+    launchOptions: {
+    slowMo: 800
+    },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
